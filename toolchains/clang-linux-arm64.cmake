6eb4545257895e5286e2f70e60a7bc6668a0f336
Aregtech <artak@aregtech.com>
2024-12-02 00:05:20 +0100

    Updated and tested toolchain files. Added new toolchain file to compile with Clang for ARM processors.

--- toolchains/clang-linux-arm64.cmake
+++ toolchains/clang-linux-arm64.cmake
@@ -1,14 +1,14 @@
-# This toolchain builds 64-bit binaries with GNU g++/gcc compiler for Linux on 64-bit ARM (AARCH64) processor.
+# This toolchain builds 64-bit binaries with Clang compiler for Linux on 64-bit ARM (AARCH64) processor.
 # Run example:
-# cmake -B ./product/cache/gnu-linux-arm64 -DCMAKE_TOOLCHAIN_FILE=/path/to/areg-sdk-demo/toolchains/gnu-linux-arm64.cmake
-# cmake --build ./product/cache/gnu-linux-arm64 -j20
+# cmake -B ./product/cache/clang-linux-arm64 -DCMAKE_TOOLCHAIN_FILE=/path/to/areg-sdk-demo/toolchains/clang-linux-arm64.cmake
+# cmake --build ./product/cache/clang-linux-arm64 -j20
 
 set(CMAKE_SYSTEM_NAME Linux)
 set(CMAKE_SYSTEM_PROCESSOR AARCH64)
 
-set(CMAKE_C_COMPILER aarch64-linux-gnu-gcc)
+set(CMAKE_C_COMPILER clang)
 set(CMAKE_C_COMPILER_TARGET aarch64-linux-gnu)
-set(CMAKE_CXX_COMPILER aarch64-linux-gnu-g++)
+set(CMAKE_CXX_COMPILER clang++)
 set(CMAKE_CXX_COMPILER_TARGET aarch64-linux-gnu)
 
 set(CMAKE_FIND_ROOT_PATH "/usr")
