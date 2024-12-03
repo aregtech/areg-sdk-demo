6742a45ad4e61fddd2c2d8f97ca39d65df71ed74
Aregtech <49253642+aregtech@users.noreply.github.com>
2024-12-03 21:19:50 +0100

    33 test areg-sdk demo with toolchains (#34)
    * Updated and tested toolchain files.
    * Added new toolchain file to compile with Clang for ARM processors.
    * Fixing cross-compile for x86 processor.

--- toolchains/clang-linux-arm32.cmake
+++ toolchains/clang-linux-arm32.cmake
@@ -1,14 +1,14 @@
-# This toolchain builds 32-bit binaries with GNU g++/gcc compiler for Linux on 32-bit ARM processor
+# This toolchain builds 32-bit binaries with Clang compiler for Linux on 32-bit ARM processor
 # Run example:
-# cmake -B ./product/cache/gnu-linux-arm32 -DCMAKE_TOOLCHAIN_FILE=/path/to/areg-sdk-demo/toolchains/gnu-linux-arm32.cmake
-# cmake --build ./product/cache/gnu-linux-arm32 -j20
+# cmake -B ./product/cache/clang-linux-arm32 -DCMAKE_TOOLCHAIN_FILE=/path/to/areg-sdk-demo/toolchains/clang-linux-arm32.cmake
+# cmake --build ./product/cache/clang-linux-arm32 -j20
 
 set(CMAKE_SYSTEM_NAME Linux)
 set(CMAKE_SYSTEM_PROCESSOR ARM)
 
-set(CMAKE_C_COMPILER arm-linux-gnueabihf-gcc)
+set(CMAKE_C_COMPILER clang)
 set(CMAKE_C_COMPILER_TARGET arm-linux-gnueabihf)
-set(CMAKE_CXX_COMPILER arm-linux-gnueabihf-g++)
+set(CMAKE_CXX_COMPILER clang++)
 set(CMAKE_CXX_COMPILER_TARGET arm-linux-gnueabihf)
 
 set(CMAKE_FIND_ROOT_PATH "/usr")
